b48b5215dde398275c4f860897975dd330274586
Leddzip <leddzip@gmail.com>
2022-12-26 22:39:47 +0100

    feature/CLOWDER-8_fully-tested-generator
    managed to have a fully tested generator written in typescript with cucumber test

--- src/app/features/step_definitions/step-def.spec.ts
+++ src/app/features/step_definitions/step-def.spec.ts
@@ -1,20 +1,34 @@
-import {Given, Then, When} from "@cucumber/cucumber";
+import {DataTable, Given, Then, When} from "@cucumber/cucumber";
 import {CustomWorld} from "./custom-world.spec";
 import assert from "assert";
 import helpers from "yeoman-test";
-import MyGeneratorToRename from "../../index";
+import * as path from "path";
 
 Given('a generator', function (this: CustomWorld) {
     // Write code here that turns the phrase above into concrete actions
     console.log("empty")
-    this.runContext = helpers.create(MyGeneratorToRename, undefined, undefined)
+    this.runContext = helpers.create(
+        // MyGeneratorToRename
+        path.join(__dirname, "../../../../generators/app")
+    )
 
 });
 
 
-When('I call it with the following prompt answer', function (this: CustomWorld, dataTable) {
+When('I call it with the following prompt answer', async function (this: CustomWorld, dataTable: DataTable) {
+    let promptSetup = {
+        name: "default"
+    };
+
+    type PromptKey = keyof typeof promptSetup;
     // Write code here that turns the phrase above into concrete actions
-    console.log("datatable", dataTable)
+     for (const [prompt, answer] of dataTable.rows()) {
+        promptSetup[prompt as PromptKey] = answer;
+     }
+     this.runContext?.withPrompts(promptSetup);
+    // this.runResult = await this.runContext?.run()
+
+    this.runResult = await this.runContext?.run();
 });
 
 
@@ -23,4 +37,10 @@ Then('I should have a file {string} with the content', function (this: CustomWor
     console.log("string", string)
     console.log("docstring", docString)
     assert(this.runContext !== undefined)
+    assert(this.runResult !== undefined)
+    this.runResult.assertFile("generated.md")
+    const content = this.runResult.fs.read("generated.md") // to retrieve content
+    console.log("content", content)
+
+    this.runResult.assertFileContent("generated.md", docString)
 });
